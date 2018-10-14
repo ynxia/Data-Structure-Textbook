cc212f0fd3c7a121d70bcaed6ca553a8064c9f49
ynxia <jason97112@163.com>
2018-10-14 09:59:37 +0800

    更新单链表

--- 第2章 线性表-课本/2.3 单链表.cpp
+++ 第2章 线性表-课本/2.3 单链表.cpp
@@ -61,6 +61,157 @@ public:
 //单链表的插入算法
 bool List::Insert(int i, int& x)
 {
-	
+	//将新元素x插入到第i个节点之后。i从一开始，i=0表示插入到第一个节点之前
+	if(first == NULL || i == 0)
+	{
+		LinkNode* newNode = new LinkNode(x);
+		if(newNode == NULL)
+		{
+			cerr  << "存储分配错误" << endl;
+			exit(1);
+		}
+		newNode -> link = first;
+		first = newNode;
+	}
+	else
+	{
+		LinkNode* current = first;
+		//循链找到第i个结点，循环结束即找到第i个结点
+		for(k = 1; k < i; k++) 
+		{
+			if(current == NULL)
+			{
+				break;
+			}
+			else
+			{
+				current = current -> link;
+			}
+		}
+		if(current == NULL)
+		{
+			cerr << "无效的插入位置" << endl;
+			return false;
+		}
+		else
+		{
+			LinkNode* newNode = new LinkNode(x);
+			if(newNode == NULL)
+			{
+				cerr << "存储分配错误" << endl;
+				exit(1);
+			}
+			newNode -> link = current -> link;
+			current -> link = newNode;
+		}
+	}
+	return true;
+};
+
+// 单链表的删除算法
+bool List::Remove(int i, int& x)
+{
+	//将链表中第i个元素删去，i从1开始、
+	if(i <= 1)
+	{
+		del = first; //删去首节点
+		first = first -> link;
+	}
+	else
+	{
+		current = first;
+		// 找到第i个结点
+		for(int k = 0; k < i; k++)
+		{
+			if(current == NULL)
+			{
+				break;
+			}
+			else
+			{
+				current = current -> link;
+			}
+		}
+		if(current == NULL)
+		{
+			cerr << "无效的删除位置" << endl;
+			exit(1);
+		}
+		del = current -> link;
+		current -> link = del -> link;
+	}
+	//取出被删除结点的值
+	x = del -> data;
+	delete del;
+	return true;
 }
+
+// 带附加头结点的单链表的插入算法
+newNode -> link = current -> link;
+current -> link = newNode;
+// 带附加头结点的单链表的删除算法
+del = current -> link;
+current -> link = del -> link;
+delete del;
+
+//带附加头结点的单链表的类定义
+template<class T>
+struct LinkNode{
+	T data;
+	LinkNode<T>* link;
+	LinkNode(LinkNode<T>* ptr = NULL) //仅初始化指针成员的构造函数
+	{
+		link = ptr;
+	}
+	LinkNode(const T& item, LinkNode<T>* ptr = NULL)
+	{
+		data = item;
+		link = ptr;
+	}
+};
+
+template<class T>
+class List:public LinearList<T>
+{
+public:
+	List()
+	{
+		first = new LinkNode<T>;
+	}
+	List(const T& x)
+	{
+		first = new LinkNode<T>(x);
+	}
+	List(List<T>& L);
+	~List()
+	{
+		makeEmpty();
+	}
+	void makeEmpty();
+	int Length() const;
+	LinkNode<T> *getHead() const //返回附加头结点地址
+	{
+		return first;
+	}
+	LinkNode<T> *Search(T x);
+	LinkNode<T> *Locate(int i);
+	bool getData(int i, T& x) const;
+	void setData(int i, T& x);
+	bool Insert(int i, T& x);
+	bool Remove(int i, T& x);
+	bool IsEmpty() const
+	{
+		return first -> link == NULL ? true:false;
+	}
+	bool IsFull() const
+	{
+		return false;
+	}
+	void Sort();
+	void input();
+	void output();
+	List<T>& operator=(List<T>& L);
+protected:
+	LinkNode<T> *first;
+};
 
